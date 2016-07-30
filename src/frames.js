620b9d77a46dc66a8451066b69f75fd2725ad77f
Mahmut Surekci <msurekci@findmypast.com>
2016-07-30 14:13:24 +0100

    add test and implementation for converting a miss

--- src/frames.js
+++ src/frames.js
@@ -1,12 +1,15 @@
 export const getValueOfRolls  = (game) => {
 	const strike = 'X';
+	const miss = '-';
 
 	const replace = () => {
-		let rolls = game.split("|");
+		let rolls = getRolls();
 
 		for(let i = 0; i < rolls.length; i++){
 			if(isStrike(rolls[i]))
 				rolls.splice(i, 1, 10);
+			else if(isMiss(rolls[i]))
+				rolls.splice(i, 1, 0);
 		}
 
 		return rolls;
@@ -14,6 +17,15 @@ export const getValueOfRolls  = (game) => {
 
 	const isStrike = (roll) => {
 		return roll.includes(strike)
+	}
+
+	const isMiss = (roll) => {
+		return roll.includes(miss);
+	}
+
+	const getRolls = () => {
+		let removeFrames = game.replace(/\|/g, "");
+		return removeFrames.split("");
 	}
 
 	return replace();
--- test/unit/frames.spec.js
+++ test/unit/frames.spec.js
@@ -4,4 +4,8 @@ describe('Frames', () => {
   it('should return 10 where there is an occurrence of X', () => {
     expect(getValueOfRolls('X|X')).to.eql([10, 10]);
   });
+
+  it('should return 0 when there is an occurrence of a -', () => {
+  	expect(getValueOfRolls('--|--')).to.eql([0, 0, 0, 0]);
+  });
 });
